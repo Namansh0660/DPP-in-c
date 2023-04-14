a77ad7798d9d0de2abd65280bb6337a41e275bff
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-04-14 23:33:31 +0530

    Find factorial of a number

---
+++ Day 3/Q2.c
@@ -0,0 +1,14 @@
+//Find factorial of a number
+#include<stdio.h>
+int main()
+{
+    int n,fact=1;
+    printf("Enter Number : ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        fact*=i;
+    }
+    printf("Factorial of %d is : %d",n,fact);
+    return 0;
+}
