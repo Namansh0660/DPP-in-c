643a3409e297710ab7245a8863354dac16b7b55e
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-04-14 23:46:12 +0530

    Half Pyramid of Numbers
    Output : 
    1
    12
    123
    1234
    12345

---
+++ Day 6/Q2.c
@@ -0,0 +1,14 @@
+//Half Pyramid of Numbers
+#include<stdio.h>
+int main()
+{
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<=i;j++)
+        {
+            printf("%d",(j+1));
+        }
+        printf("\n");
+    }
+    return 0;
+}
