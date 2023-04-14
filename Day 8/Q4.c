3c0d2ece72aa422bc8d77f8d6f42b8502706fe9c
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-04-15 00:36:01 +0530

    Find Matrix Addition using arrays

---
+++ Day 8/Q4.c
@@ -0,0 +1,37 @@
+//Find Matrix Addition using arrays
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter Size : ");
+    scanf("%d",&n);
+    int a[n][n];
+    int b[n][n];
+    int c[n][n];
+    printf("Enter elements of the first matrix:\n");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Enter elements of the first matrix:\n");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    printf("Sum of the two matrices:\n");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%d ", (a[i][j]+b[i][j]));
+        }
+        printf("\n");
+    }
+    return 0;
+}
