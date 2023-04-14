b68a171cc32cba854effe222b71fb4ec9c0a64c7
Namansh <115533264+Namansh0660@users.noreply.github.com>
2023-04-14 23:31:48 +0530

    Multipy given floating-point numbers

---
+++ Day 2/Q2.c
@@ -0,0 +1,13 @@
+//Multipy given floating-point numbers
+#include<stdio.h>
+int main()
+{
+    float a,b,pro;
+    printf("Enter value of A : ");
+    scanf("%f",&a);
+    printf("Enter value of B : ");
+    scanf("%f",&b);
+    pro=a*b;
+    printf("Product of A and B is : %0.2f\n",pro);
+    return 0;
+}
